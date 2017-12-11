c81d8c4f939c0894b2b3dd9d20201ffe81ce4d88
deelam <lam.dna@gmail.com>
2017-12-10 20:03:38 -0600

    check if port in use before trying to start; return stacktrace to REST client

--- dataengine/server/src/main/java/dataengine/server/RestParameterHelper.java
+++ dataengine/server/src/main/java/dataengine/server/RestParameterHelper.java
@@ -6,11 +6,9 @@ import java.util.concurrent.Future;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
-
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.SecurityContext;
-
 import dataengine.api.ApiResponseMessage;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,9 +21,10 @@ public final class RestParameterHelper {
 
   static Response makeBadRequestResponse(String msg) {
     log.warn("Bad request: {}", msg);
-    return Response.status(Status.BAD_REQUEST)
+    Response response = Response.status(Status.BAD_REQUEST)
         .entity(new ApiResponseMessage(ApiResponseMessage.ERROR, msg))
         .build();
+    return log(response);
   }
 
   static Response makeResponseIfIdInvalid(String idType, String id) {
@@ -47,22 +46,17 @@ public final class RestParameterHelper {
       if (responseObj.get() == null) {
         String errMsg = "Null response: " + msg;
         log.warn(errMsg);
-        response=Response.status(Status.NOT_FOUND)
-            .entity(new ApiResponseMessage(ApiResponseMessage.ERROR,
-                errMsg))
-            .build();
+        response = Response.status(Status.NOT_FOUND)
+            .entity(new ApiResponseMessage(ApiResponseMessage.ERROR, errMsg)).build();
       } else {
         response=Response.ok().entity(responseObj.get()).build();
       }
     } catch (InterruptedException | ExecutionException e) {
       String errMsg = "Exception caught during " + msg + ": " + e.getCause();
-      log.warn(errMsg, e);
-      response=Response.status(Status.INTERNAL_SERVER_ERROR)
-          .entity(new ApiResponseMessage(ApiResponseMessage.ERROR, errMsg))
-          .build();
+      response = createExceptionResponse(Status.INTERNAL_SERVER_ERROR, ApiResponseMessage.ERROR, 
+          e.getCause(), errMsg);
     }
-    log.info("REST response: {}\n\t", response, response.getEntity());
-    return response;
+    return log(response);
   }
 
   static Response makeResultResponse(String objectType, String relativeLocationUriPrefix, String id,
@@ -73,10 +67,8 @@ public final class RestParameterHelper {
       if (result == null) {
         String errMsg = "Object of type " + objectType + " not found with id: " + id;
         log.warn(errMsg);
-        response=Response.status(Status.NOT_FOUND)
-            .entity(new ApiResponseMessage(ApiResponseMessage.ERROR,
-                errMsg))
-            .build();
+        response = Response.status(Status.NOT_FOUND)
+            .entity(new ApiResponseMessage(ApiResponseMessage.ERROR, errMsg)).build();
       } else {
         response = Response.ok(result)
             .location(URI.create(relativeLocationUriPrefix + id))
@@ -84,13 +76,10 @@ public final class RestParameterHelper {
       }
     } catch (InterruptedException | ExecutionException e) {
       String errMsg = objectType + " object, id=" + id + " caused exception: " + e.getCause();
-      log.warn(errMsg);
-      response=Response.status(Status.CONFLICT)
-          .entity(new ApiResponseMessage(ApiResponseMessage.ERROR, errMsg))
-          .build();
+      response = createExceptionResponse(Status.INTERNAL_SERVER_ERROR, ApiResponseMessage.ERROR, 
+          e.getCause(), errMsg);
     }
-    log.info("REST response: {}\n\t", response, response.getEntity());
-    return response;
+    return log(response);
   }
 
   static <T> Response tryCreateObject(String objectType, T inputObj, String relativeLocationUriPrefix,
@@ -99,7 +88,7 @@ public final class RestParameterHelper {
     if (inputObj == null) {
       String errMsg = "Submitted " + objectType + " cannot be null!";
       log.warn(errMsg);
-      response=makeBadRequestResponse(errMsg);
+      return makeBadRequestResponse(errMsg);
     } else {
       try {
         String id = getId.apply(inputObj);
@@ -124,15 +113,42 @@ public final class RestParameterHelper {
                 .status(Status.CREATED).entity(newObj).build();
           }
         }
-      } catch (Exception e) { // could be Vertx msg timeout
-        String errMsg = objectType + " object cannot be created from " + inputObj + " : " + e.getCause();
-        log.warn(errMsg, e.getCause());
-        response = Response.status(Status.CONFLICT)
-            .entity(new ApiResponseMessage(ApiResponseMessage.ERROR, errMsg))
-            .build();
+      } catch (Exception e) {
+        String errMsg = objectType + " object cannot be created from " + inputObj + " : " + e;
+        response = createExceptionResponse(Status.INTERNAL_SERVER_ERROR, ApiResponseMessage.ERROR, e, errMsg);
       }
     }
-    log.info("REST response: {}\n\t", response, response.getEntity());
+    return log(response);
+  }
+
+  private static Response log(Response response) {
+    if(response.getStatus()<400)
+      log.info("REST response: {}", response);
+    else if(response.getStatus()<500)
+      log.info("REST Client error: {}", response);
+    else
+      log.warn("REST Server error: {}", response);
     return response;
+  }
+
+  private static Response createExceptionResponse(Status status, int code, Throwable e, String errMsg) {
+    log.warn("REST error: {}", errMsg, e);
+    return Response.status(status).entity(new ApiResponseMessage(code, errMsg + "\n" + toString(e)))
+        .build();
+  }
+
+  private static String toString(Throwable t) {
+    if (t == null)
+      return "null";
+    // throws NPException: org.apache.commons.lang.exception.ExceptionUtils.getStackTrace(t)
+    StringBuilder sb = new StringBuilder();
+    do {
+      sb.append(t.getClass().getName()).append(": ").append(t.getMessage());
+      for (StackTraceElement e : t.getStackTrace()) {
+        sb.append("\n\t").append(e);
+      }
+      t=t.getCause();
+    }while(t!=null);
+    return sb.toString();
   }
 }
